d2b1831c0949ecf5a534693841a637a648d96a5b
Kojo Quao <quaogeorge@icloud.com>
2026-01-06 16:53:22 +0000

    prepare project for deployment

--- budget_tracker/budget_tracker/settings.py
+++ budget_tracker/budget_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7(i)&ctw05)j-f%5pl72f$tkh+xxvw*qu)*%@*1nu=edl_i3n8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
